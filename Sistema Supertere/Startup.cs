9db532917af74d55e5c344868c24edd739578de3
mln6666 <poncemarioleo@gmail.com>
2017-03-08 21:38:33 -0300

    Add project files.

---
+++ Sistema Supertere/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sistema_Supertere.Startup))]
+namespace Sistema_Supertere
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
