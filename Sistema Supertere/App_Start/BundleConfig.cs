a713a2b84dd2b4722f06308770e1aeec310d2a15
mln6666 <poncemarioleo@gmail.com>
2017-03-08 23:39:37 -0300

    +Agregado Spacelab +Configurados scripts para release

--- Sistema Supertere/App_Start/BundleConfig.cs
+++ Sistema Supertere/App_Start/BundleConfig.cs
@@ -8,8 +8,32 @@ namespace Sistema_Supertere
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            BundleTable.EnableOptimizations = true;
+            ScriptBundle scriptBundle = new ScriptBundle("~/js");
+            string[] scriptArray =
+            {
+             "~/Scripts/jquery-1.12.4.js",
+                       "~/Scripts/jquery-1.12.4.min.js",
+                        "~/Scripts/jquery-ui-1.12.1.js",
+                       "~/Scripts/jquery-ui-1.12.1.min.js",
+                        "~/Scripts/jquery.unobtrusive-ajax.js",
+                       "~/Scripts/bootstrap.js",
+                       "~/Scripts/datatables/jquery.datatables.js",
+                       "~/Scripts/datatables/datatables.bootstrap.js",
+                       "~/Scripts/bootstrap.min.js",
+                       "~/Scripts/respond.js",
+                       "~/Scripts/bootbox.js",
+                       "~/Scripts/toastr.js",
+                       "~/Scripts/autosize.js",
+                       "~/Scripts/bootstrap-select.min.js",
+                       "~/Scripts/i18n/defaults-ar_AR.min.js",
+                       "~/Scripts/select2.js"
+        };
+
+
+            scriptBundle.Include(scriptArray);
+            scriptBundle.IncludeDirectory("~/Scripts/", "*.js");
+            bundles.Add(scriptBundle);
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -19,13 +43,13 @@ namespace Sistema_Supertere
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Styles/css").Include(
+             "~/Content/bootstrap-Spacelab.css", 
+                       "~/Content/bootstrap-select.css",
+                       "~/Content/bootstrap-select.css.map",
+                       "~/Content/datatables/css/datatables.bootstrap.css",
+                       "~/content/toastr.css",
+                        "~/Content/css/jquery-ui.min.css", "~/Content/Site.css", "~/Content/hover.css", "~/Content/css/select2.css"));
         }
     }
 }
