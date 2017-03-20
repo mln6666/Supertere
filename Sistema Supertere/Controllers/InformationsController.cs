84fd9a82a137782a1d5e320e8ebc17c6e04919a0
mln6666 <poncemarioleo@gmail.com>
2017-03-20 02:23:23 -0300

    +Modulo base de ganancias por día

---
+++ Sistema Supertere/Controllers/InformationsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sistema_Supertere.Context;
+using Sistema_Supertere.Models;
+using Sistema_Supertere.ViewModels;
+
+namespace Sistema_Supertere.Controllers
+{
+    public class InformationsController : Controller
+    {
+        private TereContext db = new TereContext();
+
+        // GET: Informations
+        [HttpPost]
+        public ActionResult DaySales(DateTime date)
+        {
+            DaySalesVM ds=new DaySalesVM();
+            ds.Date = date;
+            ds.Sales = db.Sales.ToList().FindAll(x => x.SaleDate == date & x.SaleState != SaleState.Cuenta);
+            ds.DaySalesTotal = 0;
+            ds.DayGain = 0;
+
+            foreach (var sale in ds.Sales)
+            {
+                foreach (var saleline in sale.SaleLines)
+                {
+                    ds.DaySalesTotal += saleline.LineTotal;
+                }
+                
+            }
+            ds.DayGain = ds.DaySalesTotal * 0.25m;
+
+
+            return View(ds);
+        }
+
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ Sistema Supertere/ViewModels/DaySalesVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sistema_Supertere.Models;
+
+namespace Sistema_Supertere.ViewModels
+{
+    public class DaySalesVM
+    {
+        public DateTime Date { get; set; }
+
+        public ICollection<Sale> Sales { get; set; }
+
+        public decimal DaySalesTotal { get; set; }
+
+        public decimal DayGain { get; set; }
+
+
+
+    }
+}
